65ea394b91a42d0337f134c451b7c74bdfb01d5a
Hassaan Ur Rasheed <fantastic7650@gmail.com>
2025-02-14 18:13:44 +0500

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -1,6 +1,7 @@
 import unittest
 import time_series_visualizer
 
+#testing module for time series visualizer
 class TimeSeriesTestCase(unittest.TestCase):
     def test_line_plot(self):
         fig = time_series_visualizer.draw_line_plot()
